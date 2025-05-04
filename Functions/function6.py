4c854f8c085ad6aac78c541dc2da19afdae58ea1
Raman Singh Chauhan <ramansinghchauhan787@gmail.com>
2025-05-04 18:51:25 +0545

    done

---
+++ Functions/function6.py
@@ -0,0 +1,21 @@
+#lambda function is just similar like arrow function in javascript
+#     (()=>{
+#     console.log("hello")
+#       })()
+
+# const Raman =  ()=>{
+#     console.log("HEllo")
+# }
+# Raman()
+
+
+#In python   its Syntax is 
+# lambda arguments: expression
+Raman=  lambda n : print(n)
+Raman(5)    #in python lambda functiion only accept single line code
+
+
+
+Billa = lambda a,b:  (a +b )
+result = Billa(10,5)
+print(result)
